05f824fa77700ff74513cd28aa85991d335db7d3
Xander I <immadogeuwu@gmail.com>
2024-12-05 13:11:23 -0500

    first commit

---
+++ dictionary.js
@@ -0,0 +1,134 @@
+Dictionary = [
+  {
+      "word": "Mund",
+      "uses": [
+        {
+          "type": "PROTO",
+          "compound": "",
+          "origin": "",
+          "definition": "Sky",
+          "example": "",
+          "exampleTranslation": ""
+        }
+      ]
+  },
+  {
+      "word": "Tele",
+      "uses": [
+        {
+          "type": "PROTO",
+          "compound": "",
+          "origin": "Toki Pona",
+          "definition": "Water/Liquid",
+          "example": "",
+          "exampleTranslation": ""
+        }
+      ]
+  },
+  {
+      "word": "Kind",
+      "uses": [
+        {
+          "type": "PROTO",
+          "compound": "",
+          "origin": "Toki Pona",
+          "definition": "Stone",
+          "example": "",
+          "exampleTranslation": ""
+        }
+      ]
+  },
+  {
+      "word": "Vaer",
+      "uses": [
+        {
+          "type": "PROTO",
+          "compound": "",
+          "origin": "",
+          "definition": "Fire",
+          "example": "",
+          "exampleTranslation": ""
+        }
+      ]
+  },
+  {
+      "word": "Tala",
+      "uses": [
+        {
+          "type": "PROTO",
+          "compound": "",
+          "origin": "",
+          "definition": "Plant",
+          "example": "",
+          "exampleTranslation": ""
+        }
+      ]
+  },
+  {
+    "word": "Tale",
+    "uses": [
+      {
+        "type": "PROTO",
+        "compound": "",
+        "origin": "",
+        "definition": "Light",
+        "example": "",
+        "exampleTranslation": ""
+      }
+    ]
+},
+{
+  "word": "Tale-ala",
+  "uses": [
+    {
+      "type": "Compound",
+      "compound": "Tale + Ala",
+      "origin": "",
+      "definition": "Dark",
+      "example": "",
+      "exampleTranslation": ""
+    }
+  ]
+},
+{
+  "word": "Kyaf",
+  "uses": [
+    {
+      "type": "PROTO",
+      "compound": "",
+      "origin": "",
+      "definition": "Air/Gas",
+      "example": "",
+      "exampleTranslation": ""
+    }
+  ]
+},
+{
+  "word": "Ma",
+  "uses": [
+    {
+      "type": "PROTO",
+      "compound": "",
+      "origin": "",
+      "definition": "Land",
+      "example": "",
+      "exampleTranslation": ""
+    }
+  ]
+},
+
+  {
+      "word": "janweli",
+      "uses": [
+          {
+              "type": "COMPOUND",
+              "compound": "jan + soweli",
+              "definition": "furry (animal person)",
+              "example": "sina janweli.",
+              "exampleTranslation": "You are a furry."
+          }
+      ]
+  }
+]
+
+Dictionary.sort((a, b) => a.word.localeCompare(b.word));
